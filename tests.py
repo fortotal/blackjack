4da295656d83f2ebf9adfa093723f644e3a1919c
Stepan Kutsenogiy <kutsenogiystepan@gmail.com>
2021-02-25 01:31:17 +0700

    [back] to right tests

--- tests.py
+++ tests.py
@@ -7,5 +7,5 @@ def test_player_logic():
 	simple_player = pl.Player(16)
 	while simple_player.get_status():
 		simple_player.get_card(deck)
-	assert simple_player.current_value() < 15
+	assert simple_player.current_value() >= 16
 
